87345452be5fe2e3480fbef74de75172794b8ae0
Anushka-shukla <shuklaannushka@gmail.com>
2022-01-31 13:39:56 +0530

    some changes

--- script.js
+++ script.js
@@ -1,32 +1,9 @@
-// nav scroll
-const links = document.querySelectorAll(".page-header ul a");
-
-for (const link of links) {
-  link.addEventListener("click", clickHandler);
-}
-
-function clickHandler(e) {
-  e.preventDefault();
-  const href = this.getAttribute("href");
-  const offsetTop = document.querySelector(href).offsetTop;
-
-  scroll({
-    top: offsetTop,
-    behavior: "smooth"
-  });
-}
-
-
-// $(document).ready(function(){ 
-//     $(window).scroll(function(){ 
-//         if ($(this).scrollTop() > 100) { 
-//             $('#scroll').fadeIn(); 
-//         } else { 
-//             $('#scroll').fadeOut(); 
-//         } 
-//     }); 
-//     $('#scroll').click(function(){ 
-//         $("html, body").animate({ scrollTop: 0 }, 600); 
-//         return false; 
-//     }); 
-// });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click",
+        function(scroll) {
+            scroll.preventDefault();
+            document.querySelector(this.getAttribute("href")).scrollIntoView({
+                behavior: "smooth"
+            });
+        });
+});
